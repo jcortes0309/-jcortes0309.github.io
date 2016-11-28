b1fc6e7ab01a1451b661cb23fd271c2d5c404e77
Juan A Cortes <juan.cortes@gmail.com>
2016-11-27 23:54:39 -0500

    Make changes to navigation

--- js/custom.js
+++ js/custom.js
@@ -22,3 +22,32 @@ $(document).scroll(function() {
     }
   }
 });
+
+// Opens menu bar
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+// Closes menu bar
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+
+  var list = document.getElementsByClassName("menu")[0];
+  // console.log(list);
+  list.getElementsByClassName("active")[0].classList.remove("active");
+  setActive();
+}
+
+
+function setActive() {
+  console.log("Called the setActive function");
+  aObj = document.getElementById("nav").getElementsByTagName('a');
+  for(i = 0; i < aObj.length; i++) {
+    if(document.location.href.indexOf(aObj[i].href) >= 0) {
+      console.log(aObj[i]);
+      aObj[i].className="active";
+    }
+  }
+}
+
+window.onload = setActive;
